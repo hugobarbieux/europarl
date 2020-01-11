db90e5cddba9d1223f68d76e54667f1d17ca63c4
hugobarbieux <43614788+hugobarbieux@users.noreply.github.com>
2020-01-11 22:43:04 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,7 +17,7 @@ print "The first 2000 characters are: ", xmldata[0:]
 root = lxml.etree.fromstring(xmldata)
 
 # this line uses xpath to find <text> tags
-lines = root.findall('.//text[@font="9"]//b')
+lines = root.findall('.//text[@font="9"]//text')
 print lines
 for line in lines:
     print line.text
