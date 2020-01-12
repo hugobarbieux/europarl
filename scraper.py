b41d812a72dd035cd9c73a3698e756d2e893ca0e
hugobarbieux <43614788+hugobarbieux@users.noreply.github.com>
2020-01-12 01:54:30 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,7 +15,7 @@ print "All pdf characters are: ", xmldata[0:]
 root = lxml.etree.fromstring(xmldata)
 
 # this line uses xpath, to find <text> tags
-lines = root.findall('.//text[@height="13"]')
+lines = root.findall('.//text[@height="13"]//b')
 print lines
 for line in lines:
     print line.text
